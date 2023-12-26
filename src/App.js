c8944f0d8b7c493a327eeb29194b76105031abb9
yeskay1011 <mrsaravanan1012@gmail.com>
2023-12-26 16:17:42 +0530

    signup page

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Home/Home";
 import Booking from "./Booking/Booking";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import ConfirmBooking from "./Booking/Confirm";
 import Sucess from "./Sucess";
+import Signup from "./Signup/Signup";
 
 function App() {
   return (
@@ -13,10 +14,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Signup />} />
           <Route path="/bookticket" element={<Booking />} />
           <Route path="/confirmbook" element={<ConfirmBooking />} />
           <Route path="/success" element={<Sucess />} />
-
         </Routes>
       </BrowserRouter>
     </>
